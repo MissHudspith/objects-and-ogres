fbba8fa69740466f3de6707c60c8198c8c44456a
Miss Hudspith <ahudspith@clvweb.co.uk>
2025-06-25 10:11:51 +0000

    Created basic character class

--- part1.py
+++ part1.py
@@ -1,1 +1,17 @@
-print("Hello")
+class Character:
+    def __init__(self, name):
+        self.name = name
+        self.health = 10
+        self.strength = 1
+        self.defence = 1
+
+    def getStats(self):
+        return "".join([self.name, "\n", "♥:", str(self.health), "\n", "💪:", str(self.strength), "\n", "⛊:", str(self.defence)])
+
+# Main code starts here
+p1 = Character(input("Enter p1 name: "))
+print(p1.getStats())
+print("")
+
+p2 = Character(input("Enter p2 name: "))
+print(p2.getStats())
